fb637faf2cea4ac297a2664d80fa333f46ac567b
dimasqr <dimasquintana1@gmail.com>
2025-04-05 23:19:20 -0600

    Feat: Adding Semana6 files

--- backend/app.js
+++ backend/app.js
@@ -12,13 +12,10 @@ var usersRouter = require("./routes/users");
 var app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://habits-tracker-three.vercel.app",
     credentials: true,
   })
 );
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -42,7 +39,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({ error: err });
 });
 
 module.exports = app;
